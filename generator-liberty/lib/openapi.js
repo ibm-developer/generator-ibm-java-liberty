497bbe60ca29811a15702b4a7d7c98cb6fd12efa
Paul W Bennett <pwbennet@us.ibm.com>
2018-03-26 16:05:34 -0500

    feat: Use SDKGen for Liberty BFF

--- generator-liberty/lib/openapi.js
+++ generator-liberty/lib/openapi.js
@@ -16,19 +16,27 @@
 
 // module for generating code from open api document
 
-'use strict'
+'use strict';
 
+const request = require('request')
+const Promise = require('bluebird')
+const unzip = require('unzip2')
+const requestAsync = Promise.promisify(request)
 const fs = require('fs')
 const path = require('path')
 const os = require('os')
-const appAccUrl = 'https://liberty-app-accelerator.wasdev.developer.ibm.com'
-const appAccEndpoint = appAccUrl + '/start/api/v1'
-const unzip = require('unzip2')
-const request = require('request')
-const Promise = require('bluebird')
+const log = require('ibm-java-codegen-common').log
 Promise.promisifyAll(request)
 
-const generate = function (docs) {
+//const sdkGenURL = 'https://mobilesdkgen.ng.bluemix.net/sdkgen/api/generator/'
+const sdkGenURL = 'https://staging.devex.bluemix.net:443/sdkgen/api/generator/'
+const sdkGenCheckDelay = 3000
+
+let logger = log
+
+const generate = function (docs, parentLogger) {
+  logger = parentLogger || log
+  logger.writeToLog('Liberty Generator generating code from open api document: ' + JSON.stringify(docs,null,2))
   const openApiDir = []
   const p = new Promise((resolve) => {
     let i = 0
@@ -46,50 +54,107 @@ const generate = function (docs) {
 }
 
 const generateFromDoc = function (doc) {
-  let id = undefined
-  return request.getAsync({
-    url: appAccEndpoint + '/workspace'
-  }).then(response => {
-    id = response.body
-    return request.postAsync({
-      url: appAccEndpoint + '/upload?tech=swagger&cleanup=true&process=true&workspace=' + id,
-      method: 'POST',
-      formData: {
-        'swaggerDefinition': {
-          value: doc,
-          options: {filename: 'swaggerDefinition', contentType: 'application/json'}
-        }
-      }
+  return performSDKGenerationAsync('libertySDK', 'server_java_jaxrs_spec', doc)
+    .then(generatedID => {
+      logger.writeToLog('Liberty Generator generated code from open api document with id ' + generatedID)
+      return getServerSDKAsync('libertySDK', generatedID)
     })
-  }).then(response => {
-    if (!response.body.includes('success')) {
-      throw new Error('Open api file upload failed.')
-    }
-    return getGeneratedContent(id)
-  })
-}
-
-const getGeneratedContent = function (id) {
-  return new Promise((resolve, reject) => {
-    const {sep} = require('path')
-    const tempDir = fs.mkdtempSync(os.tmpdir() + sep)
-    request.get({
-      headers: {'Accept': 'application/zip'},
-      url: appAccEndpoint + '/workspace/files?workspace=' + id + '&serviceId=swagger&dir=server'
-    })
-      .on('error', err => {
-        reject(new Error('Getting files from app accelerator failed with: ', err.message))
-      })
-      .pipe(unzip.Extract({path: tempDir}))
-      .on('close', () => {
-        resolve(tempDir)
-      })
-  })
 }
 
 const writeFiles = function (dirs, generator) {
-  dirs.forEach(tempDir => {
-    generator.fs.copy(path.join(tempDir, 'swagger', 'server', 'src'), generator.destinationPath('src'))
+  dirs.forEach(sdk => {
+    generator.fs.copy(path.join(sdk.tempDir, 'generated-code', 'JavaJaxRS-Spec', 'src', 'gen'), generator.destinationPath('src/main'))
+    generator.fs.delete(generator.destinationPath('src/main/java/io/swagger/api/RestApplication.java'))
+
+  })
+}
+
+const performSDKGenerationAsync = function (sdkName, sdkType, fileContent) {
+  const startGenURL = `${sdkGenURL}${sdkName}/${sdkType}`
+  logger.writeToLog(`starting SDK generation job for ${sdkName} using ${startGenURL}`)
+  return request.postAsync({
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Openapi-Opts': ''
+    },
+    url: startGenURL,
+    body: fileContent
+  })
+    .then(function (response) {
+      const body = JSON.parse(response.body)
+      logger.writeToLog('SDK generation job response body: ' + JSON.stringify(body, null, 2))
+      if (body.job && body.job.id) {
+        return body.job.id
+      }
+      throw new Error('SDK generation error:', response.statusCode, response.statusMessage, body.message)
+    })
+    .tap(generatedID => logger.writeToLog(`SDK generation job for ${sdkName} started with id ${generatedID}`))
+    .then(generatedID => checkUntilFinished(generatedID))
+
+  function checkUntilFinished (generatedID, count) {
+    count = count || 1;
+    logger.writeToLog(`#${count} checking status of SDK generation job with id ${generatedID} (for ${sdkName})`);
+    return getStatusAsync(generatedID)
+      .then(finished => {
+        if (finished) {
+          logger.writeToLog(`SDK generation job with id ${generatedID} (for ${sdkName}) is complete`)
+          return generatedID
+        } else {
+          if (count <= 10) {
+            return Promise.delay(sdkGenCheckDelay).then(() => checkUntilFinished(generatedID, count + 1))
+          } else {
+            throw new Error('Timeout error, couldn\'t generate SDK within timeout.')
+          }
+        }
+      })
+  }
+}
+
+function getStatusAsync (generatedID) {
+  const getStatusURL = `${sdkGenURL}${generatedID}/status`
+  return requestAsync({
+    headers: {'Accept': 'application/json'},
+    url: getStatusURL
+  })
+    .then(response => {
+      logger.writeToLog('SDK generation status response: ' + JSON.stringify(response, null, 2))
+      const status = JSON.parse(response.body).status
+      switch (status) {
+        case 'FINISHED':
+          return true
+
+        case 'VALIDATION_FAILED':
+        case 'FAILED':
+          throw new Error('SDK generator creation failed with status: ', status)
+
+        default:
+          return false
+      }
+    })
+}
+
+const getServerSDKAsync = function (sdkName, generatedID) {
+  const serverDownloadURL = sdkGenURL + generatedID
+  // Use the non-async version of request.get() here because
+  // we are going to use .pipe() to stream the data to disk
+  return new Promise((resolve, reject) => {
+    const {sep} = require('path')
+    const tempDir = fs.mkdtempSync(os.tmpdir() + sep)
+    logger.writeToLog(`starting server SDK download and unzip for ${sdkName} from ${serverDownloadURL} to ${tempDir}`)
+    request.get({
+      headers: {'Accept': 'application/zip'},
+      url: serverDownloadURL
+    })
+      .on('error', err => {
+        reject(new Error('Getting server SDK failed with error: ', err.message))
+      })
+      .pipe(unzip.Extract({path: tempDir}))
+      .on('close', () => {
+        logger.writeToLog(`finished server SDK download and unzip for ${sdkName} from ${serverDownloadURL} to ${tempDir}`)
+
+        resolve({tempDir: tempDir, dirname: sdkName})
+      })
   })
 }
 
